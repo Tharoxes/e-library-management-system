72973a94c9bb89633e523eb6a90ceea37e3a8dbc
Thanh <thc.huynh@protonmail.com>
2024-03-11 13:26:49 +0100

    Init backend
    spring initializr - web app, sql, LMS

---
+++ backend/src/main/java/com/tharoxes/elibrarymanagementsystem/ELibraryManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.tharoxes.elibrarymanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ELibraryManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ELibraryManagementSystemApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/tharoxes/elibrarymanagementsystem/ELibraryManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tharoxes.elibrarymanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ELibraryManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
